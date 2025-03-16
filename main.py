839f7fb00fc1e5f03804aff635aac2c9c1f54d68
schiwnn26 <codmasterd@gmail.com>
2025-03-16 00:17:32 -0400

    edited stats.py and main.py to now write out nicely and in order the total count of words and each letter.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #import the needed function(s) from stats.py
 from stats import get_num_words
 from stats import get_num_characters
+from stats import sort_char_count
 
 def get_book_text(filepath):
     #set variable(s)
@@ -17,12 +18,31 @@ def main():
     #print the contents of the set txt file (commented out below line)
     #print(get_book_text('books/frankenstein.txt'))
 
-    total_characters_dict = get_num_characters('books/frankenstein.txt')
-
     #print the number of words in the set txt file
-    print(f"{get_num_words('books/frankenstein.txt')} words found in the document")
-    
-    print(total_characters_dict)
+    #print(f"{get_num_words('books/frankenstein.txt')} words found in the document")
 
-main()
+    #print(total_characters_dict)
+
+    total_characters_dict = get_num_characters('books/frankenstein.txt')
+    sorted_chars = sort_char_count(total_characters_dict) 
+    
+     # Print the report
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {get_num_words('books/frankenstein.txt')} total words")
+    print("--------- Character Count -------")
+    
+    # Print each character and its count
+    for char_dict in sorted_chars:
+        char = char_dict["char"]
+        count = char_dict["count"]
+        # Only print alphabetical characters
+        if char.isalpha():
+            print(f"{char}: {count}")
+    
+    print("============= END ===============")
+
+if __name__ == "__main__":
+    main()
 
--- stats.py
+++ stats.py
@@ -15,13 +15,34 @@ def get_num_words(filepath):
     return num_words
 
 def get_num_characters(filepath):
+    #set variable(s)
     dict_letter={}
+
+    #open the file to be read
     with open(filepath) as f:
+        #read the file
         str_file = f.read()
+        #loop over each character in the string. if the character exist add 1 to the count for that dictionary key, if not make a new dictionary key and set the count to 1
         for character in str_file:
             character_lower = character.lower()
             if character_lower in dict_letter:
                 dict_letter[character_lower]+=1
             else:
                 dict_letter[character_lower]=1
+        #return the dictionary
         return dict_letter
+    
+def sort_char_count(char_count):
+    # Create a list of dictionaries
+    char_list = []
+    for char, count in char_count.items():
+        char_list.append({"char": char, "count": count})
+    
+    # Define a helper function for sorting
+    def sort_on(dict_item):
+        return dict_item["count"]
+    
+    # Sort the list
+    char_list.sort(reverse=True, key=sort_on)
+    
+    return char_list
